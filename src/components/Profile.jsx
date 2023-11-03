6f08390ee69044cb2adaa721a9f89314881edda2
Muhtasin Jawad <mutasin.jawad@g.bracu.ac.bd>
2023-11-03 12:28:04 +0600

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import defaultPic from '../assets/profile.png';
 
 const Profile = () => {
@@ -6,16 +6,31 @@ const Profile = () => {
     const [profilePicSrc, setProfilePicSrc] = useState(defaultPic);
 
     const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      // Read the selected file as a data URL
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setProfilePicSrc(e.target.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
+        const file = event.target.files[0];
+        if (file) {
+        // Read the selected file as a data URL
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            setProfilePicSrc(e.target.result);
+        };
+        reader.readAsDataURL(file);
+        }
+    };
+
+    useEffect(() => {
+        const phoneInput = document.getElementById('phone');
+    
+        if (phoneInput) {
+          phoneInput.addEventListener('input', function () {
+            const valid = phoneInput.checkValidity();
+            if (valid) {
+              // Phone number is valid
+            } else {
+              // Phone number is not valid
+            }
+          });
+        }
+        }, []);
 
     return (
     <div className='flex flex-col items-center'>
@@ -47,14 +62,21 @@ const Profile = () => {
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                         First Name
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-                    <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Jane" />
                     </div>
                     <div class="w-full md:w-1/2 px-3">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                         Last Name
                     </label>
                     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+                    </div>
+                </div>
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-phone">
+                        Phone
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="phone" type="tel" placeholder="01XX-XXXX-XXX" pattern="01[3-9][0-35-9]-\d{4}-\d{3}" required/>
                     </div>
                 </div>
                 <div class="flex flex-wrap -mx-3 mb-6">
