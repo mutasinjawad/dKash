3383ba3d4fdf93400f5f881d6ab7ccde380bbd4a
Mr.Khawaja <alveehawak360@gmail.com>
2023-11-18 01:31:13 +0600

    transactions

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -1,75 +1,115 @@
-import React, { useEffect, useState } from 'react';
-import {Link, useParams} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import host from "../api";
 
-const UserHome = ({token, user, setUser}) => {
+const UserHome = ({ token, user, setUser }) => {
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    fetch(host + "/profile", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((err) => console.log(err));
+    fetch(host + "/money/transactions", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    })
+      .then((data) => data.json())
+      .then((data) => setTransactions(data))
+      .catch((err) => console.log(err));
+  }, [token]);
 
-    useEffect(() => {
-        fetch(host + "/profile", {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              "token": token,
-            },
-          }).then((data) => data.json()).then((data) => {setUser(data)})
-            .catch((err) => console.log(err));
-    }
-    , [token]);
-
-
-    return(
+  return (
+    <div>
+      <div className="flex justify-between px-[230px] my-[80px] w-[full]">
         <div>
-            <div className='flex justify-between px-[230px] my-[80px] w-[full]'>
-                <div>
-                    <h1 className='font-[600] text-[20px] p-2'>BALANCE</h1>
-                    <p className='font-[700] text-[80px]'>&#2547; {user.balance} BDT</p>
-                </div>
-                <div className='flex flex-col'>
-                    <div className='flex mb-[40px]'>
-                        {/* Add Money */}
-                        <Link to='/add'>
-                            <div className='mr-10 sm:mt-0'>
-                                <div
-                                    className='flex justify-center items-center bg-[#e8deff] w-[200px] h-[80px] rounded-[25px] shadow group hover:bg-primaryColor cursor-pointer ease-in duration-300'>
-                                        <h3 className='text-primaryColor font-[500] text-[17px] group-hover:text-white'>Add Money <i class="ri-map-pin-add-line"></i></h3>
-                                </div>
-                            </div>
-                        </Link>
-                        {/* send Money */}
-                        <Link to='/send'>
-                            <div className='ml-10 sm:mt-0'>
-                                <div
-                                    className='flex justify-center items-center bg-[#e8deff] w-[200px] h-[80px] rounded-[25px] shadow group hover:bg-primaryColor cursor-pointer ease-in duration-300'>
-                                        <h3 className='text-primaryColor font-[500] text-[17px] group-hover:text-white'>Send Money <i class="ri-send-plane-line"></i></h3>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className='flex'>
-                        {/* Cashout */}
-                        <div className='mr-10 sm:mt-0'>
-                            <div
-                                className='flex justify-center items-center bg-[#e8deff] w-[200px] h-[80px] rounded-[25px] shadow group hover:bg-primaryColor cursor-pointer ease-in duration-300'>
-                                    <h3 className='text-primaryColor font-[500] text-[17px] group-hover:text-white'>Cashout <i class="ri-logout-circle-r-line"></i></h3>
-                            </div>
-                        </div>
-                        {/* Payment */}
-                        {user.type !== 'agent' && (<div className='ml-10 sm:mt-0'>
-                            <div
-                                className='flex justify-center items-center bg-[#e8deff] w-[200px] h-[80px] rounded-[25px] shadow group hover:bg-primaryColor cursor-pointer ease-in duration-300'>
-                                    <h3 className='text-primaryColor font-[500] text-[17px] group-hover:text-white'>Payment <i class="ri-secure-payment-line"></i></h3>
-                            </div>
-                        </div>)}
-                    </div>
-                </div>
-            </div>
-            <div className='flex justify-between px-[230px] bg-smallTextColor h-[518px] w-full'>
-                <h1 className='font-[600] text-[20px] text-white pt-10'>RECENT</h1>
-                <div></div>
-            </div>
+          <h1 className="font-[600] text-[20px] p-2">BALANCE</h1>
+          <p className="font-[700] text-[80px]">&#2547; {user.balance} BDT</p>
         </div>
-
-    )
+        <div className="flex flex-col">
+          <div className="flex mb-[40px]">
+            {/* Add Money */}
+            <Link to="/add">
+              <div className="mr-10 sm:mt-0">
+                <div className="flex justify-center items-center bg-[#e8deff] w-[200px] h-[80px] rounded-[25px] shadow group hover:bg-primaryColor cursor-pointer ease-in duration-300">
+                  <h3 className="text-primaryColor font-[500] text-[17px] group-hover:text-white">
+                    Add Money <i class="ri-map-pin-add-line"></i>
+                  </h3>
+                </div>
+              </div>
+            </Link>
+            {/* send Money */}
+            <Link to="/send">
+              <div className="ml-10 sm:mt-0">
+                <div className="flex justify-center items-center bg-[#e8deff] w-[200px] h-[80px] rounded-[25px] shadow group hover:bg-primaryColor cursor-pointer ease-in duration-300">
+                  <h3 className="text-primaryColor font-[500] text-[17px] group-hover:text-white">
+                    Send Money <i class="ri-send-plane-line"></i>
+                  </h3>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="flex">
+            {/* Cashout */}
+            <div className="mr-10 sm:mt-0">
+              <div className="flex justify-center items-center bg-[#e8deff] w-[200px] h-[80px] rounded-[25px] shadow group hover:bg-primaryColor cursor-pointer ease-in duration-300">
+                <h3 className="text-primaryColor font-[500] text-[17px] group-hover:text-white">
+                  Cashout <i class="ri-logout-circle-r-line"></i>
+                </h3>
+              </div>
+            </div>
+            {/* Payment */}
+            {user.type !== "agent" && (
+              <div className="ml-10 sm:mt-0">
+                <div className="flex justify-center items-center bg-[#e8deff] w-[200px] h-[80px] rounded-[25px] shadow group hover:bg-primaryColor cursor-pointer ease-in duration-300">
+                  <h3 className="text-primaryColor font-[500] text-[17px] group-hover:text-white">
+                    Payment <i class="ri-secure-payment-line"></i>
+                  </h3>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between px-[230px] bg-smallTextColor h-[518px] w-full">
+        <h1 className="font-[600] text-[20px] text-white pt-10">RECENT</h1>
+        <div>
+          <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b">Sender ID</th>
+                <th className="py-2 px-4 border-b">Receiver ID</th>
+                <th className="py-2 px-4 border-b">Transaction Type</th>
+                <th className="py-2 px-4 border-b">Amount</th>
+                <th className="py-2 px-4 border-b">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((transaction) => (
+                <tr key={transaction.date}>
+                  <td className="py-2 px-4 border-b">{transaction.sender}</td>
+                  <td className="py-2 px-4 border-b">{transaction.receiver}</td>
+                  <td className="py-2 px-4 border-b">{transaction.type}</td>
+                  <td className="py-2 px-4 border-b">{transaction.amount}</td>
+                  <td className="py-2 px-4 border-b">{transaction.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserHome;
