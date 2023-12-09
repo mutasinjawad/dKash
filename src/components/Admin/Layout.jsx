9c6cb44c261dfec697862f0341c925e8329282ca
Mr.Khawaja <alveehawak360@gmail.com>
2023-12-09 12:21:30 +0600

    Update Layout.jsx

--- src/components/Admin/Layout.jsx
+++ src/components/Admin/Layout.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from './Sidebar';
-import Header from './Header';
+import Sidebar from "./Sidebar";
+import Header from "./Header";
 
-const Layout = ({token, user, setUser}) => {
-
-    return(
-        <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
-            <Sidebar />
-            <div className='flex-1'>
-                <Header token={token} user={user} setUser={setUser}/>
-                {<Outlet token={token} user={user} setUser={setUser}/>}
-            </div>
-        </div>
-    )
+const Layout = ({ token, user, setUser }) => {
+  return (
+    <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
+      <Sidebar />
+      <div className="flex-1" style={{ overflowY: "scroll" }}>
+        <Header token={token} user={user} setUser={setUser} />
+        {<Outlet token={token} user={user} setUser={setUser} />}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
